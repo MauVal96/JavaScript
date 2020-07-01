879e6c83293f031c737cfe7787f91de220b751bc
MauVal96 <m.valadezolmos@ugto.mx>
2020-07-01 16:27:06 -0500

    Funciones

---
+++ funciones.js
@@ -0,0 +1,11 @@
+var nombre = "Mauricio", edad = 23;
+
+function imprimirEdad(name,age) {
+    console.log(`${name} tiene ${age} años`);
+}
+
+imprimirEdad(nombre, edad);
+imprimirEdad('Carlos', 28);
+imprimirEdad('Vicky', 24);
+imprimirEdad('Dario', 27);
+imprimirEdad('Eric', 24);
